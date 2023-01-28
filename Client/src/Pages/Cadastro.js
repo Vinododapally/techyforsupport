94853f9ef1bfed9754ecbcd93ade3ab2efa3c02b
Vinod <odapallyvinod@gmail.com>
2023-01-28 15:04:44 +0530

    initial commit

---
+++ Client/src/Pages/Cadastro.js
@@ -0,0 +1,109 @@
+import "../Styles/cadastro.css"
+import Img from "../Assets/result.svg"
+import * as yup from "yup";
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import Axios from "axios";
+import { Link } from 'react-router-dom';
+
+const loca_api = "http://localhost:8080"
+const remote_api = "http://techyforsupport-env.eba-y3nm7sqv.ap-northeast-1.elasticbeanstalk.com:8080"
+
+function Cadastro({ logado = false }) {
+    const handleRegister = (values) => {
+        Axios.post(loca_api+"/register", {
+            email: values.email,
+            password: values.password,
+        }).then((response) => {
+            alert(response.data.msg);
+            window.location.reload();
+        });
+    };
+
+    const validationsRegister = yup.object().shape({
+        email: yup
+            .string()
+            .email("E-mail invalid")
+            .required("email id is required"),
+        password: yup
+            .string()
+            .min(8, "Password must be atleast 8 characters")
+            .required("please enter password"),
+        confirmation: yup
+            .string()
+            .oneOf([yup.ref("password"), null], "password does not match")
+            .required("Password must be atleast 8 characters"),
+    });
+
+
+    return (
+        <div className="body">
+            <div className="left-cadastro">
+                <img src={Img} alt="Pessoas olhando gráficos" className="chart" />
+            </div>
+            <div className="right-cadastro">
+                <div className="card-cadastro">
+                    <div className="user-links">
+                        <div className="user-link-home">
+                            {!logado && <Link to="/">Already have one? Login</Link>}
+                        </div>
+
+                        {/* <div className="user-link-cad">
+                            {!logado && <Link to="/cadastro">Register</Link>}
+                        </div> */}
+                    </div>
+                    <h1>Sign up</h1>
+                    <Formik
+                        initialValues={{}}
+                        onSubmit={handleRegister}
+                        validationSchema={validationsRegister}
+                    >
+                        <Form className="login-form">
+                            <div className="form-group">
+                                <label form="email">Email</label>
+
+                                <Field name="email" type='email' className="form-field" placeholder="Email" />
+
+                                <ErrorMessage
+                                    component="span"
+                                    name="email"
+                                    className="form-error"
+                                />
+                            </div>
+
+                            {/*Outro campo*/}
+
+                            <div className="form-group">
+                                <label form="email">Password</label>
+                                <Field name="password" type='password' className="form-field" placeholder="Password" autoComplete="on"/>
+
+                                <ErrorMessage
+                                    component="span"
+                                    name="password"
+                                    className="form-error"
+                                />
+                            </div>
+
+                            {/*Confirmação*/}
+
+                            <div className="form-group">
+                                <label form="email">Confirm Password</label>
+                                <Field name="confirmation" type='password' className="form-field" placeholder="Confirm Password" autoComplete="on"/>
+
+                                <ErrorMessage
+                                    component="span"
+                                    name="confirmation"
+                                    className="form-error"
+                                />
+                            </div>
+                            <button className="button" type="submit">
+                                Register
+                            </button>
+                        </Form>
+                    </Formik>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cadastro;
---
+++ Client/src/Pages/Login.js
@@ -0,0 +1,123 @@
+import "../Styles/login.css"
+import * as yup from "yup";
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import Axios from "axios";
+import Img from "../Assets/result.svg"
+import { Link } from 'react-router-dom';
+
+const loca_api = "http://localhost:8080"
+const remote_api = "http://techyforsupport-env.eba-y3nm7sqv.ap-northeast-1.elasticbeanstalk.com:8080"
+
+function Login({logado=false}) {
+  const handleLogin = (values) => {
+    Axios.post(loca_api+"/login", {
+      email: values.email,
+      password: values.password,
+    }).then((response) => {
+      const page = response.data;
+      if (page === true) {
+        localStorage.setItem('@user', JSON.stringify(response.config.data));
+        window.location.reload();
+      } else {
+        alert(response.data.msg);
+      }
+
+    });
+  };
+
+  const handleRegister = (values) => {
+    Axios.post(loca_api+"/register", {
+      email: values.email,
+      password: values.password,
+    }).then((response) => {
+      alert(response.data.msg);
+      console.log(response);
+    });
+  };
+
+  const validationsLogin = yup.object().shape({
+    email: yup
+      .string()
+      .email("email invalid")
+      .required("please enter email Id"),
+    password: yup
+      .string()
+      .min(8, "password must be 8 characters atleast")
+      .required("please enter password"),
+  });
+
+  const validationsRegister = yup.object().shape({
+    email: yup
+      .string()
+      .email("email invalid")
+      .required("please enter email Id"),
+    password: yup
+      .string()
+      .min(8, "password must be 8 characters atleast")
+      .required("please enter password"),
+    confirmation: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "password does not match")
+      .required("please enter confirm password"),
+  });
+
+  return (
+    <div className="body">
+      <div className="left-login">
+        <img src={Img} alt="Pessoas olhando gráficos" className="chart" />
+      </div>
+
+      <div className="right-login">
+        <div className="card-login">
+          <div className="user-links">
+            {/* <div className="user-link-home">
+              {!logado && <Link to="/">Home</Link>}
+            </div> */}
+
+            <div className="user-link-cad">
+              {!logado && <Link to="/register"> Dont have one? Register</Link>}
+            </div>
+          </div>
+          <h1>Sign in</h1>
+          <Formik
+            initialValues={{}}
+            onSubmit={handleLogin}
+            validationSchema={validationsLogin}
+          >
+            <Form className="login-form">
+              <div className="form-group">
+                <label form="email">Email</label>
+
+                <Field name="email" type='email' className="form-field" placeholder="Email" />
+
+                <ErrorMessage
+                  component="span"
+                  name="email"
+                  className="form-error"
+                />
+              </div>
+
+              {/*Outro campo*/}
+
+              <div className="form-group">
+                <label form="email">Password</label>
+                <Field name="password" type='password' className="form-field" placeholder="Password" autoComplete="on"/>
+
+                <ErrorMessage
+                  component="span"
+                  name="password"
+                  className="form-error"
+                />
+              </div>
+              <button className="button" type="submit">
+                Login
+              </button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
